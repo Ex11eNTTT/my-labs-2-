84491a48a28d14bb4a3ff039b4298fb8412598f3
Дмитрий Гаврилов <lina2282253945@gmail.com>
2024-09-18 18:04:03 +0700

    Стили

--- app.py
+++ app.py
@@ -28,12 +28,14 @@ def author():
 @app.route("/lab1/oak")
 def oak():
     path = url_for("static", filename="oak.jpg")
+    path_css = url_for("static", filename='lab1.css')
     return'''
 <!doctype html>
+<link rel="stylesheet" href="'''+path_css+'''">
 <html>
     <body>
         <h1>Дуб</h1>
-        <img src="''' +path+ '''">
+        <img class='image' src="''' +path+ '''">
     </body>
 </html>
 '''
