db2a17ee9bfc3268c1140ab321f6888c7438512a
Дмитрий Гаврилов <lina2282253945@gmail.com>
2024-10-02 12:01:43 +0700

    Вывод цветка

--- app.py
+++ app.py
@@ -525,3 +525,13 @@ def a():
 @app.route('/lab2/a/')
 def a2():
     return 'со слешем'
+
+
+flower_list=('роза','тюльпан','незабудка','ромашка')
+
+@app.route('/lab2/flowers/<int:flower_id>')
+def flowers(flower_id):
+    if flower_id >= len(flower_list):
+        return "такого цветка нет", 404
+    else:
+        return "цветок: " + flower_list[flower_id]
