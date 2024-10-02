2c6fbbced1e7d27b6ced3f61f821a88bdb8e6b59
Дмитрий Гаврилов <lina2282253945@gmail.com>
2024-10-02 12:08:58 +0700

    Добавление цветка

--- app.py
+++ app.py
@@ -527,7 +527,7 @@ def a2():
     return 'со слешем'
 
 
-flower_list=('роза','тюльпан','незабудка','ромашка')
+flower_list=['роза','тюльпан','незабудка','ромашка']
 
 @app.route('/lab2/flowers/<int:flower_id>')
 def flowers(flower_id):
@@ -535,3 +535,19 @@ def flowers(flower_id):
         return "такого цветка нет", 404
     else:
         return "цветок: " + flower_list[flower_id]
+    
+
+@app.route('/lab2/ad_flower/<name>')
+def addflower(name):
+    flower_list.append(name)
+    return f'''
+<!doctype html>
+<html>
+    <body>
+    <h1>Добавлен новый цветок<h1>
+    <p>Название нового цветка: {name}</p>
+    <p>Всего цветов: {len(flower_list)}</p>
+    <p>Полный список: {flower_list}</p>
+    </body>
+</html>
+'''
