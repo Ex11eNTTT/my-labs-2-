cd3a3106f261afa4b301d1a82d1be39ce76089cd
Дмитрий Гаврилов <lina2282253945@gmail.com>
2024-10-17 14:26:15 +0700

    Cookie

--- lab3.py
+++ lab3.py
@@ -1,6 +1,25 @@
-from flask import Blueprint, url_for, redirect, render_template, request
+from flask import Blueprint, url_for, redirect, render_template, request, make_response
 lab3 = Blueprint('lab3', __name__)
 
 @lab3.route('/lab3/')
 def lab():
-    return render_template('/lab3/lab3.html')
+    name = request.cookies.get('name')
+    name_color = request.cookies.get('color')
+    return render_template('/lab3/lab3.html', name=name, name_color=name_color)
+
+
+@lab3.route('/lab3/cookie')
+def lab_cockie():
+    resp = make_response('Установки куки!', 200)
+    resp.set_cookie('name', 'Alex', max_age=5)
+    resp.set_cookie('age', '20')
+    resp.set_cookie('color', 'magenta')
+    return resp
+
+@lab3.route('/lab3/delete_cookie')
+def lab_delte_cookie():
+    resp = make_response(redirect('/lab3/'))
+    resp.delete_cookie('name')
+    resp.delete_cookie('age')
+    resp.delete_cookie('color')
+    return resp
