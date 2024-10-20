4c4aae187bfd0ce95fb6373092be23bb8bd40b5b
Дмитрий Гаврилов <lina2282253945@gmail.com>
2024-10-20 14:23:49 +0700

    Делал

--- lab3.py
+++ lab3.py
@@ -4,14 +4,15 @@ lab3 = Blueprint('lab3', __name__)
 @lab3.route('/lab3/')
 def lab():
     name = request.cookies.get('name')
+    age = request.cookies.get('age')
     name_color = request.cookies.get('color')
-    return render_template('/lab3/lab3.html', name=name, name_color=name_color)
+    return render_template('/lab3/lab3.html', name=name, name_color=name_color, age=age)
 
 
 @lab3.route('/lab3/cookie')
 def lab_cockie():
-    resp = make_response('Установки куки!', 200)
-    resp.set_cookie('name', 'Alex', max_age=5)
+    resp = make_response(redirect('/lab3/'))
+    resp.set_cookie('name', 'Alex', max_age=300)
     resp.set_cookie('age', '20')
     resp.set_cookie('color', 'magenta')
     return resp
