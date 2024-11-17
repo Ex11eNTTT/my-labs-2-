ec651838ce931b5ec9e8ebeb5c0469c59ec44f5e
Дмитрий Гаврилов <lina2282253945@gmail.com>
2024-11-17 15:44:05 +0700

    Исправление SQL-инъекции

--- lab5.py
+++ lab5.py
@@ -35,13 +35,13 @@ def register():
 
     login = request.form.get('login')
     password = request.form.get('password')
-    cur.execute(f"SELECT login FROM users WHERE login = '{login}';")
+    cur.execute(f"SELECT login FROM users WHERE login = %s;", (login,))
     if cur.fetchone():
         db_close(conn,cur)
         return render_template('lab5/register.html',
                                 error='Такой пользователь уже существует')
     password_hash = generate_password_hash(password)
-    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password_hash}');")
+    cur.execute(f"INSERT INTO users (login, password) VALUES (%s, %s);", (login,password_hash))
     db_close(conn,cur)
     return render_template('lab5/success.html', login=login)
 
@@ -59,7 +59,7 @@ def login():
 
 
     
-    cur.execute(f"SELECT * FROM users WHERE login='{login}';")
+    cur.execute(f"SELECT * FROM users WHERE login=%s;", (login,))
     user = cur.fetchone()
 
     if not user:
@@ -92,7 +92,7 @@ def create():
     cur.execute("SELECT * FROM users WHERE login=%s;", (login,))
     login_id = cur.fetchone()["id"]
 
-    cur.execute(f"INSERT INTO articles(user_id, title, article_text) VALUES ({login_id}, '{title}','{article_text}');")
+    cur.execute(f"INSERT INTO articles(user_id, title, article_text) VALUES (%s, %s, %s);", (login_id, title, article_text))
     db_close(conn, cur)
     return redirect('/lab5')
 
@@ -104,10 +104,10 @@ def list():
         return redirect('/lab5/login')
     
     conn, cur = db_connect()
-    cur.execute(f"SELECT id FROM users WHERE login='{login}';")
+    cur.execute(f"SELECT id FROM users WHERE login=%s;", (login,))
     login_id = cur.fetchone()['id']
     
-    cur.execute(f"SELECT * FROM articles WHERE user_id='{login_id}';")
+    cur.execute(f"SELECT * FROM articles WHERE user_id=%s;", (login_id,))
     articles = cur.fetchall()
 
     db_close(conn, cur)
