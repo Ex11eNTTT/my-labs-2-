54b7bb951ff043bb5b0592e0df5f6a339f716934
Дмитрий Гаврилов <lina2282253945@gmail.com>
2024-11-25 18:25:10 +0700

    Заготовка к 6-ой лабораторной

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
+from lab6 import lab6
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'Никому не говори пжпж')
