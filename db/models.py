4667237a29c130df8264e2150e2767357e5a7d1b
Дмитрий Гаврилов <lina2282253945@gmail.com>
2024-12-13 15:19:56 +0700

    1

--- db/models.py
+++ db/models.py
@@ -7,8 +7,9 @@ class users(db.Model):
 
 class articles(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    login_id = db.Column(db.Integer, db. ForeignKey('users.id'))
     title = db.Column(db.String(50), nullable=False)
     article_text = db.Column(db.Text, nullable=False)
     is_favorite = db.Column(db.Boolean)
-    is_public = db.Column(db.Boolean, nullable=False, default=False)
+    is_public = db.Column(db.Boolean)
+    likes = db.Column(db.Integer)
