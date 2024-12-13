5d25b04d22db952b7b228949730bcbd262659220
Дмитрий Гаврилов <lina2282253945@gmail.com>
2024-12-13 15:43:41 +0700

    Регистрация

--- lab8.py
+++ lab8.py
@@ -2,8 +2,12 @@ from flask import Blueprint, url_for, redirect, render_template, request, make_r
 import sqlite3
 import json
 from os import path
+from db import db
+from db.models import users, articles
 from datetime import datetime
 import psycopg2
+from werkzeug.security import check_password_hash, generate_password_hash
+
 from psycopg2.extras import RealDictCursor
 lab8 = Blueprint('lab8', __name__)
 
@@ -11,3 +15,22 @@ lab8 = Blueprint('lab8', __name__)
 @lab8.route('/lab8/')
 def main_l8():
     return render_template('lab8/lab8.html')
+
+
+@lab8.route('/lab8/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    login_exists = users.query.filter_by(login=login_form).first()
+    if login_exists:
+        return render_template('lab8/register.html', error='Такой пользователь уже существует')
+
+    password_hash = generate_password_hash(password_form)
+    new_user = users(login=login_form, password=password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/lab8/')
